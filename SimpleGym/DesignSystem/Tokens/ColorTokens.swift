1528cbcd535a5ea4fdb3102eb14a813650eb2ea2
VladSyutin <syutin.asd@yandex.ru>
2026-04-18 21:14:17 +0300

    Refine calendar and liquid glass control colors

--- SimpleGym/DesignSystem/Tokens/ColorTokens.swift
+++ SimpleGym/DesignSystem/Tokens/ColorTokens.swift
@@ -1,13 +1,27 @@
 import SwiftUI
 
 enum ColorTokens {
+    private static func dynamicColor(light: UIColor, dark: UIColor) -> Color {
+        Color(
+            uiColor: UIColor { traitCollection in
+                traitCollection.userInterfaceStyle == .dark ? dark : light
+            }
+        )
+    }
+
     static let accentBlue = Color(red: 0 / 255, green: 136 / 255, blue: 255 / 255)
-    static let accentBlueSoft = Color(red: 214 / 255, green: 236 / 255, blue: 255 / 255)
+    static let accentBlueSelectionBackground = dynamicColor(
+        light: UIColor(red: 214 / 255, green: 236 / 255, blue: 255 / 255, alpha: 1),
+        dark: UIColor(red: 0 / 255, green: 136 / 255, blue: 255 / 255, alpha: 0.12)
+    )
     static let backgroundPrimary = Color(uiColor: .systemBackground)
     static let labelPrimary = Color(uiColor: .label)
     static let labelSecondary = Color(uiColor: .secondaryLabel)
     static let labelTertiary = Color(uiColor: .tertiaryLabel)
-    static let vibrantControlPrimary = Color(red: 64 / 255, green: 64 / 255, blue: 64 / 255)
+    static let labelVibrantControlPrimary = dynamicColor(
+        light: UIColor(red: 64 / 255, green: 64 / 255, blue: 64 / 255, alpha: 1),
+        dark: UIColor.label
+    )
     static let separator = Color(uiColor: .separator)
     static let white = Color.white
 }
